31107a4c92649351c7d45ffb241da6998610a564
Mikhail Kucherenko <mihail.k.work@mail.ru>
2022-11-05 22:35:08 +0300

    1.base-concepts-1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,39 @@
 function solveEquation(a, b, c) {
   let arr;
   // код для задачи №1 писать здесь
-  return arr; // array
+  
+  "use strict";
+
+  const discriminant = b ** 2 - 4 * a * c;
+  if (discriminant < 0) {
+    arr = [];
+  } else if (discriminant === 0) {
+    arr = [-b / (2 * a)];
+  } else if (discriminant > 0) {
+    arr = [(-b + Math.sqrt(discriminant)) / (2 * a), (-b - Math.sqrt(discriminant)) / (2 * a)];
+  }
+    return arr; // array
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
-
   // код для задачи №2 писать здесь
+  "use strict";
 
-  return totalAmount;
+  const loanBody = amount - contribution;
+
+  let now = new Date();
+  let nowDate = Date.parse(now);
+
+  let afterDate = Date.parse(date);
+    
+  let month = Math.round((afterDate - nowDate) / (1000 * 60 * 60 * 24 * 30));
+  console.log(month);
+
+  let P = percent / 12 / 100;
+  let monthPay = loanBody * (P + (P / (((1 + P) ** month) - 1)));
+  
+  totalAmount = monthPay * month;
+ 
+  return totalAmount.toFixed(2);
 }
