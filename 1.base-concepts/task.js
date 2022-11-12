e98d4aca1fae6c1c7bfcf1d5f5050ded3ac47733
Mikhail Kucherenko <mihail.k.work@mail.ru>
2022-11-12 18:12:21 +0300

    1.base-concepts-2

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,16 +1,16 @@
+"use strict";
+
 function solveEquation(a, b, c) {
   let arr;
   // код для задачи №1 писать здесь
-  
-  "use strict";
-
+   
   const discriminant = b ** 2 - 4 * a * c;
-  if (discriminant < 0) {
-    arr = [];
-  } else if (discriminant === 0) {
-    arr = [-b / (2 * a)];
+  if (discriminant === 0) {
+    arr = [- b / (2 * a)];
   } else if (discriminant > 0) {
     arr = [(-b + Math.sqrt(discriminant)) / (2 * a), (-b - Math.sqrt(discriminant)) / (2 * a)];
+  } else {
+    arr = [];
   }
     return arr; // array
 }
@@ -18,7 +18,6 @@ function solveEquation(a, b, c) {
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
   // код для задачи №2 писать здесь
-  "use strict";
 
   const loanBody = amount - contribution;
 
@@ -27,13 +26,12 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
 
   let afterDate = Date.parse(date);
     
-  let month = Math.round((afterDate - nowDate) / (1000 * 60 * 60 * 24 * 30));
-  console.log(month);
-
+  let month = Math.round((afterDate - nowDate) / (1000 * 60 * 60 * 24 * 30.5));
+  
   let P = percent / 12 / 100;
   let monthPay = loanBody * (P + (P / (((1 + P) ** month) - 1)));
   
   totalAmount = monthPay * month;
  
-  return totalAmount.toFixed(2);
+  return +totalAmount.toFixed(2);
 }
