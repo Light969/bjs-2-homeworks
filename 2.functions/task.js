78639ae6ddfcc71200195f76321f64298c672988
Mikhail Kucherenko <mihail.k.work@mail.ru>
2022-11-13 19:49:19 +0300

    functions-1

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -4,14 +4,38 @@ function getArrayParams(arr) {
 
   // Ваш код
 
+  sum = 0;
+  min = Infinity;
+  max = -Infinity;
+
+  for (i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+
+  for (i = 0; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+    } else if (arr[i] > max) {
+      max = arr[i];
+    }
+
+  avg = +(sum / arr.length).toFixed(2);
+  }
+
   return { min: min, max: max, avg: avg };
 }
+
+
 
 // Задание 2
 function worker(arr) {
   let sum;
 
   // Ваш код
+  sum = 0;
+  for (i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
 
   return sum;
 }
@@ -20,6 +44,12 @@ function makeWork(arrOfArr, func) {
   let max;
 
   // Ваш кода
+  max = 0;
+  for (i = 0; i < arrOfArr[i].length; i++) {
+    if (worker(arrOfArr[i]) > max) {
+      max = worker(arrOfArr[i]);
+    } 
+  }
   // for ...
   
   return max;
