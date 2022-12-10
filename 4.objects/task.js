749e553d928b9235ab17e6af355dde1ee9e529d7
Mikhail Kucherenko <mihail.k.work@mail.ru>
2022-12-10 10:24:37 +0300

    Objects-2

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -30,14 +30,38 @@ Student.prototype.addMarks = function(...mark) {
   }
 }
 
+Student.prototype.getAverage = function() {
+  let total = 0;
+  let grades = this.marks;
+  for (let i = 0; i < grades.length; i++) {        
+    total += grades[i];     
+  }           
+  let avg = total / grades.length;      
+  return avg;
+}
+
+Student.prototype.exclude = function(reason) {
+  this.excluded = reason;
+  delete this.subject;
+  delete this.marks;
+}
+
 student1.setSubject("Algebra");
 student2.setSubject("Geometry");
+student3.setSubject("Geographic");
 
 student1.addMark(5);
 student1.addMark(3);
-
+student2.addMarks(4);
 student3.addMarks(2, 3, 2);
 
 console.log(student1);
 console.log(student2);
+console.log(student3);
+
+console.log(student1.getAverage()); 
+console.log(student2.getAverage());
+console.log(student3.getAverage());
+
+student3.exclude('Low grades');
 console.log(student3);
