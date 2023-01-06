2675c53c94d031881fb83ce7baf2c5eab74b4f3d
Mikhail Kucherenko <mihail.k.work@mail.ru>
2023-01-06 19:38:30 +0300

    Decorators-1

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,5 +1,24 @@
+sum = (...args) => args.reduce((arg, item) => arg + item, 0);
+const cachedSum = cachingDecoratorNew(sum);
+
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = [];
+
+  function wrapper(...args) {
+    const hash = args.join(','); // получаем правильный хэш
+    // console.log(hash);
+    let objectInCache = cache.find((item) => item === hash); // ищем элемент, хэш которого равен нашему хэшу
+    if (objectInCache) { // если элемент не найден (! удалил перед названием функции)
+      console.log("Из кэша: " + cache[hash]); // индекс нам известен, по индексу в массиве лежит объект, как получить нужное значение?
+      return "Из кэша: " + cache[hash];
+    }
+
+    let result = func(...args); // в кэше результата нет - придётся считать
+
+    cache.push({ hash: `${hash}`, value: sum(...args) }); // добавляем элемент с правильной структурой
+    // console.log(cache);
+  }
+  return wrapper;
 }
 
 
