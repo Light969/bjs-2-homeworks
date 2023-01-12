5da29168604e2d11c565135276e851df28b6354e
Mikhail Kucherenko <mihail.k.work@mail.ru>
2023-01-12 21:58:14 +0300

    Async-5

--- 7.async/task.js
+++ 7.async/task.js
@@ -4,25 +4,28 @@ class AlarmClock {
     this.timerId = null;
   }
 
-  addClock(time, text, id) {
+  addClock(time, func, id) {
     if (id === undefined) {  
       throw new Error('Параметр id не передан');
     } else if (this.alarmCollection.some(clock => clock.id === id)) {
       console.error('Будильник с таким id уже присутствует');
     } else {
-      this.alarmCollection.push({ time, text, id });
+      this.alarmCollection.push({ time, func, id });
       console.log('Новый будильник');
     }
   }
 
   removeClock(id) {
-    debugger;
+    let l = this.alarmCollection.length;
     if (this.alarmCollection.filter(clock => clock.id != id)) {
       this.alarmCollection = this.alarmCollection.filter(clock => clock.id != id);
+	  if (this.alarmCollection.length === l) {
+		  console.log('Будильник не найден');
+		  return this.alarmCollection;
+	  }  
       console.log('Будильник успешно удалён');
-    } else {
-      console.log('Будильник не найден')
-    }
+	    return this.alarmCollection;
+    } 
   }
 
   getCurrentFormattedTime() {
@@ -34,10 +37,19 @@ class AlarmClock {
     return now;
   }
 
-  // Не работает
+  start() {
+    if (this.timerId === null) { 
+      return console.log('Значение отсутствует');  
+    } this.alarmCollection.forEach(func(clock => clock.time != getCurrentFormattedTime()) { console.log(clock.time); });
+  } 
+
   // start() {
-  //   if (this.timerId === null) { 
-  //     this.alarmCollection.forEach(checkClock(clock => clock.time === getCurrentFormattedTime()) {this.timerId = clock.func}); 
+  //   if (this.timerId === null) {
+  //     // this.alarmCollection.forEach(checkClock(clock => clock.time === getCurrentFormattedTime()) {this.timerId = clock.func}); 
+
+  //     // this.timerId = this.alarmCollection[0].func;
+  //     // console.log(this.timerId);
+  //     console.log('Будильник');
   //   }
   // }
 
